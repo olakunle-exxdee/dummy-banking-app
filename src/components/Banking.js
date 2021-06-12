2316769091733fdd15d757568702886275b4ebe1
unknown <you@example.com>
2021-06-12 22:01:55 +0100

    added alert

--- src/components/Banking.js
+++ src/components/Banking.js
@@ -7,9 +7,11 @@ import {
   DEPOSIT,
   WITHDRAWAL,
 } from "../redux/bankingSlice";
+import Alert from "./Alert";
 
 const Banking = () => {
   const [amount, setAmount] = useState("");
+  const [alert, setAlert] = useState({ show: false, type: "", msg: "" });
   const handleAmount = (e) => {
     const re = /^[0-9\b]+$/;
     if (e.target.value === "" || re.test(e.target.value)) {
@@ -18,31 +20,47 @@ const Banking = () => {
   };
 
   const dispatch = useDispatch();
+
   const handleDeposit = () => {
-    dispatch(
-      DEPOSIT({
-        balance: amount,
-      })
-    );
-    setAmount("");
+    if (!amount) {
+      showAlert(true, "danger", "enter a value");
+    } else {
+      dispatch(
+        DEPOSIT({
+          balance: amount,
+        })
+      );
+      showAlert(true, "success", `$${amount} has been deposited`);
+      setAmount("");
+    }
   };
   const handleWithdraw = () => {
-    dispatch(
-      WITHDRAWAL({
-        balance: amount,
-      })
-    );
-    setAmount("");
+    if (!amount) {
+      showAlert(true, "danger", "enter a value");
+    } else {
+      dispatch(
+        WITHDRAWAL({
+          balance: amount,
+        })
+      );
+      showAlert(true, "success", `$${amount} has been withdraw`);
+      setAmount("");
+    }
   };
   const handleInterest = () => {
+    showAlert(true, "success", `$ interest has been added 🚀🚀🚀`);
     dispatch(COLLECT_INTEREST());
   };
   const handleDelete = () => {
+    showAlert(true, "success", `$ account has been deleted`);
     dispatch(DELETE_ACCOUNT());
   };
 
   const handleAccounttype = () => {
     dispatch(ACCOUNT_TYPE());
+  };
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
   };
   return (
     <div className="form-group">
@@ -52,6 +70,8 @@ const Banking = () => {
         className="form-control"
         onChange={handleAmount}
       />
+      {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+
       <button onClick={handleDeposit} className="btn btn-success">
         deposit
       </button>
